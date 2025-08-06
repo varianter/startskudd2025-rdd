32914add32eced7915e70babe48a54c47d2eaabf
robines <espi.robin@gmail.com>
2025-08-06 11:28:56 +0200

    Colorize row instead of circle

--- src/components/sensor.tsx
+++ src/components/sensor.tsx
@@ -1,18 +1,15 @@
 import { SensorDocument } from "@/types/elastic";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 
-function getStatusColor(status: string) {
+function getRowStyle(status: string) {
   switch (status) {
-    case "ON":
-      return "bg-green-500";
     case "OFF":
-      return "bg-black";
+      return "bg-gray-200 hover:bg-gray-200";
     case "ERROR":
-      return "bg-red-500";
+      return "bg-red-300 hover:bg-red-300";
     default:
-      return "bg-gray-400";
+      return "";
   }
 }
 
@@ -33,13 +30,12 @@ export function SensorTable({ sensors }: { sensors: SensorDocument[] }) {
     </TableHeader>
     <TableBody>
       {sensors.map((sensor) => (
-        <TableRow key={sensor.sensorId}>
+        <TableRow key={sensor.sensorId} className={getRowStyle(sensor.status)}>
           <TableCell>
             {capitalizeFirstLetter(sensor.sensorId.replace("-", " "))}
           </TableCell>
           <TableCell>
             <div className="flex items-center gap-1">
-              <div className={cn("rounded-full h-3 w-3", getStatusColor(sensor.status))}/>
               {sensor.status}
             </div>
           </TableCell>
