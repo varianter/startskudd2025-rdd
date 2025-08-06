f986cb0ab435920ab6692035046c009ef635832a
robines <espi.robin@gmail.com>
2025-08-06 11:02:00 +0200

    Update sensor interface

--- src/types/elastic.ts
+++ src/types/elastic.ts
@@ -1,5 +1,18 @@
+export interface SensorPosition {
+  x: number;
+  y: number;
+  depthInMeter: number;
+}
+
+export interface Sensor {
+  id: string;
+  placement: SensorPosition;
+}
+
 export interface SensorDocument {
   sensorId: string;
   status: string;
   readingDate: string;
+  sensor: Sensor;
+  deltaMovementInMm: number;
 }
