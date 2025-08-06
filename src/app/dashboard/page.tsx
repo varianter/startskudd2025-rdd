9061b9bdc0edb053d85fe7f6219e9f970fd0fa11
robines <espi.robin@gmail.com>
2025-08-06 11:23:09 +0200

    Don't render raw sensor data, log in console instead.
    * Adjust refetch interval to 5s
    * Don't show "Connected" badge, only show when fetching data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,22 +5,28 @@ import Typography from "@/components/ui/typography";
 import { useQuery } from "@tanstack/react-query";
 import { SensorDocument } from "@/types/elastic";
 import { SensorTable } from "@/components/sensor";
+import { useEffect } from "react";
 
 
 export default function Dashboard() {
-  const { data, isPending } = useQuery({
+  const { data, isFetching, isError } = useQuery({
     queryKey: ['sensors'],
     queryFn: async () => {
       const response = await fetch("/api/sensor-data");
       return await response.json();
     },
-    refetchInterval: 2000,
+    retry: 3,
+    refetchInterval: 5000,
   });
 
   const isConnected = !!data;
   const sensors: SensorDocument[] = data?.sensors;
-  const clusterName = data?.clusterName;
-  const docCount = data?.docCount;
+
+  useEffect(() => {
+    if (sensors) {
+      console.debug(sensors); // TODO: remove
+    }
+  }, [sensors]);
 
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
@@ -30,53 +36,15 @@ export default function Dashboard() {
         <Card x-chunk="dashboard-01-chunk-5">
           <CardHeader>
             <CardTitle className="gap-2 flex">
-              Connection status
-              {isPending
-                ? (<Badge variant="secondary">Connecting...</Badge>)
-                : (
-                  isConnected
-                    ? <Badge variant="default">Connected</Badge>
-                    : <Badge variant="destructive">Not connected</Badge>
-                )
-              }
+              Sensor Statuses
+              {isFetching && <Badge variant="secondary">Updating...</Badge>}
+              {isError && <Badge variant="destructive">Error fetching data</Badge>}
             </CardTitle>
           </CardHeader>
           <CardContent className="grid gap-8">
-            {isConnected ? (
-              <>
-                <p>
-                  Connected to cluster{" "}
-                  <code className="bg-slate-200 p-1 font-mono rounded-sm">
-                    {clusterName}
-                  </code>
-                  . Counting {docCount} documents
-                </p>
-
-                <div>
-                  <Typography variant="h2" className="mb-2">Sensor Statuses</Typography>
-
-                  <SensorTable sensors={sensors} />
-
-                </div>
-
-                <div className="space-y-2">
-                  <Typography variant="h2">Latest Documents for Debugging</Typography>
-                  {sensors.map((doc, idx) => (
-                    <div
-                      key={idx}
-                      className="bg-slate-100 p-2 rounded border border-slate-300"
-                    >
-                      <pre className="text-xs overflow-x-auto">
-                        {JSON.stringify(doc, null, 2)}
-                      </pre>
-                    </div>
-                  ))}
-                </div>
-              </>
-
-            ) : (
-              <p>Not connected</p>
-            )}
+            {isConnected
+              ? <SensorTable sensors={sensors}/>
+              : <p>Not connected</p>}
           </CardContent>
         </Card>
       </div>
