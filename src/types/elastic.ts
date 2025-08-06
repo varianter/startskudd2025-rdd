190ae18dc984ba7c1a542a7aca8f08ed3b9820a6
robines <espi.robin@gmail.com>
2025-08-06 14:05:26 +0200

    Collapse search result by sensorId

--- src/types/elastic.ts
+++ src/types/elastic.ts
@@ -14,5 +14,6 @@ export interface SensorDocument {
   status: string;
   readingDate: string;
   sensor: Sensor;
-  deltaMovementInMm: number;
+  deltaMovementInMm?: number;
+  readingPlacement?: SensorPosition;
 }
