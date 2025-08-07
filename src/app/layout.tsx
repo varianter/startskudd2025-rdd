f9e2a9e5bad9da8043e4a30a0188102be9e31ec1
robines <espi.robin@gmail.com>
2025-08-07 14:10:54 +0200

    Use current year in footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           <footer>
             <hr className="border-slate-300 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span className="block p-8 text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2024{" "}
+              © {new Date().getFullYear()}{" "}
               <a href="https://rigidbauta.no/" className="hover:underline">
                 Rigid Bauta AS
               </a>
